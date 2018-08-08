a05ad7c82275fe91dfa61919cbed832632919d3f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-08-08 22:37:47 +0200

    365 Certificate
    + Validity of 365 days for certificates

--- docker/jonas-thelemann.de/certs/New-Certificates.ps1
+++ docker/jonas-thelemann.de/certs/New-Certificates.ps1
@@ -16,13 +16,13 @@ $ProjectKey = Join-Path -Path $PSScriptRoot -ChildPath "$ProjectName.key"
 
 If ((-Not (Test-Path -Path $CaCrt)) -And (-Not (Test-Path -Path $CaKey))) {
     Write-Host "Creating CA certificate & private key..." -ForegroundColor "Cyan"
-    openssl req -config $CaCnf -keyout $CaKey -new -out $CaCrt -passout pass:$ClearPassword -x509
+    openssl req -config $CaCnf -days 365 -keyout $CaKey -new -out $CaCrt -passout pass:$ClearPassword -x509
 } Else {
     Write-Host "CA certificate & private key already exist." -ForegroundColor "Cyan"
 }
 
 Write-Host "Creating server certificate & private key..." -ForegroundColor "Cyan"
-openssl req -config $ProjectCnf -keyout $ProjectKey -new -nodes -out $ProjectCsr
+openssl req -config $ProjectCnf -days 365 -keyout $ProjectKey -new -nodes -out $ProjectCsr
 
 Write-Host "Signing with CA..." -ForegroundColor "Cyan"
 openssl x509 -CA $CaCrt -CAkey $CaKey -extensions x509_ext -extfile $ProjectCnf -in $ProjectCsr -out $ProjectCrt -passin pass:$ClearPassword -req -set_serial (Get-Random)
