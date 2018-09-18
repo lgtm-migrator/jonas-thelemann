8a3ba65659a6390d5a1035b7781916c8ffcbd035
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-09-18 14:18:56 +0200

    Format PHP

--- src/static/resources/dargmuesli/navigation/tableofcontents.php
+++ src/static/resources/dargmuesli/navigation/tableofcontents.php
@@ -1,40 +1,40 @@
 <?php
     use PHPHtmlParser\Dom;
 
-    function get_table_of_contents_html($content, $titleFallback = null)
-    {
-        $dom = new Dom();
-        $dom->load($content, ['whitespaceTextNode' => false, 'preserveLineBreaks' => true]);
-        $types = ['section', 'li'];
-        $parts = array();
-        $tocHtml = '';
+function get_table_of_contents_html($content, $titleFallback = null)
+{
+    $dom = new Dom();
+    $dom->load($content, ['whitespaceTextNode' => false, 'preserveLineBreaks' => true]);
+    $types = ['section', 'li'];
+    $parts = array();
+    $tocHtml = '';
 
-        foreach ($types as $type) {
-            $parts = $dom->find($type);
-
-            if (count($parts) > 0) {
-                break;
-            }
-        }
-
-        $tocHtml = '
-        <ul class="sidenav sidenav-fixed z-depth-0 section table-of-contents" id="toc-mobile">';
+    foreach ($types as $type) {
+        $parts = $dom->find($type);
 
         if (count($parts) > 0) {
-            foreach ($parts as $part) {
-                $title = $part->find('h2')[0];
+            break;
+        }
+    }
 
-                $tocHtml .= '
+    $tocHtml = '
+        <ul class="sidenav sidenav-fixed z-depth-0 section table-of-contents" id="toc-mobile">';
+
+    if (count($parts) > 0) {
+        foreach ($parts as $part) {
+            $title = $part->find('h2')[0];
+
+            $tocHtml .= '
                 <li>
                     <a href="#'.trim($part->getAttribute('id')).'">
                         '.trim(is_null($title) ? $titleFallback : $title->text).'
                     </a>
                 </li>';
-            }
         }
+    }
 
-        $tocHtml .= '
+    $tocHtml .= '
         </ul>';
 
-        return $tocHtml;
-    }
+    return $tocHtml;
+}
