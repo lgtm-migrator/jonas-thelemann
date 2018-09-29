4f715b8db0a10be8e1fd602791d213dd53c7af2a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-09-29 04:26:36 +0200

    Environment Database
    + Database name from environment variable file
    - Post parameter "dbhName"

--- src/static/resources/dargmuesli/database/rankings.php
+++ src/static/resources/dargmuesli/database/rankings.php
@@ -2,107 +2,101 @@
     include_once $_SERVER['DOCUMENT_ROOT'].'/resources/packages/composer/autoload.php';
     include_once $_SERVER['DOCUMENT_ROOT'].'/resources/dargmuesli/database/pdo.php';
     include_once $_SERVER['DOCUMENT_ROOT'].'/resources/dargmuesli/database/whitelist.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/resources/dargmuesli/filesystem/environment.php';
     include_once $_SERVER['DOCUMENT_ROOT'].'/resources/dargmuesli/text/markuplanguage.php';
 
-    $dbhName = $_POST['dbhName'];
+    // Load .env file
+    load_env_file($_SERVER['SERVER_ROOT'].'/credentials');
 
-    if (empty($dbhName)) {
-        $dbhName = null;
-    }
+    // Get right PDO instance
+    $dbh = get_dbh($_ENV['PGSQL_DATABASE']);
 
-    // Check if required parameters are given
-    if ($dbhName != null) {
+    // Continue only if PDO instance was found
+    if ($dbh != null) {
+        $tableName = $_POST['tableName'];
 
-        // Get right PDO instance
-        $dbh = get_dbh($dbhName);
+        if (empty($tableName)) {
+            $tableName = null;
+        }
 
-        // Continue only if PDO instance was found
-        if ($dbh != null) {
-            $tableName = $_POST['tableName'];
+        if (!in_array($tableName, array_keys($tableWhitelist))) {
+            throw new Exception('"'.$tableName.'" is not whitelisted!');
+        }
 
-            if (empty($tableName)) {
-                $tableName = null;
-            }
+        $rankingType = $_POST['rankingType'];
 
-            if (!in_array($tableName, array_keys($tableWhitelist))) {
-                throw new Exception('"'.$tableName.'" is not whitelisted!');
-            }
+        if (empty($rankingType)) {
+            $rankingType = null;
+        }
 
-            $rankingType = $_POST['rankingType'];
+        $sql = null;
 
-            if (empty($rankingType)) {
-                $rankingType = null;
-            }
+        switch ($rankingType) {
+            case 'list':
+                $columnName = $_POST['columnName'];
 
-            $sql = null;
+                if (empty($columnName)) {
+                    $columnName = null;
+                }
 
-            switch ($rankingType) {
-                case 'list':
-                    $columnName = $_POST['columnName'];
+                if (!in_array($columnName, $tableWhitelist[$tableName])) {
+                    throw new Exception('"'.$columnName.'" is not whitelisted!');
+                }
 
-                    if (empty($columnName)) {
-                        $columnName = null;
-                    }
+                $limit = $_POST['limit'];
 
-                    if (!in_array($columnName, $tableWhitelist[$tableName])) {
-                        throw new Exception('"'.$columnName.'" is not whitelisted!');
-                    }
+                if (empty($limit)) {
+                    $limit = null;
+                }
 
-                    $limit = $_POST['limit'];
+                if ($tableName != null && $columnName != null && $limit != null) {
+                    $sql = 'SELECT '.$columnName.', count(*) AS "quantity" FROM '.$tableName.' GROUP BY '.$columnName.' ORDER BY "quantity" DESC LIMIT '.filter_var($limit, FILTER_SANITIZE_NUMBER_INT);
+                }
 
-                    if (empty($limit)) {
-                        $limit = null;
-                    }
+                break;
+            case 'matrix':
+                $categories = json_decode($_POST['categories'], true);
 
-                    if ($tableName != null && $columnName != null && $limit != null) {
-                        $sql = 'SELECT '.$columnName.', count(*) AS "quantity" FROM '.$tableName.' GROUP BY '.$columnName.' ORDER BY "quantity" DESC LIMIT '.filter_var($limit, FILTER_SANITIZE_NUMBER_INT);
-                    }
+                if (empty($categories)) {
+                    $categories = null;
+                }
 
-                    break;
-                case 'matrix':
-                    $categories = json_decode($_POST['categories'], true);
+                // Continue only if categories are set
+                if ($tableName != null && $categories != null) {
+                    $sql = '';
 
-                    if (empty($categories)) {
-                        $categories = null;
-                    }
+                    for ($i = 0; $i < count($categories); ++$i) {
+                        $categoryKey = array_keys($categories[$i])[0];
+                        $categoryName = $categories[$i][$categoryKey];
 
-                    // Continue only if categories are set
-                    if ($tableName != null && $categories != null) {
-                        $sql = '';
-
-                        for ($i = 0; $i < count($categories); ++$i) {
-                            $categoryKey = array_keys($categories[$i])[0];
-                            $categoryName = $categories[$i][$categoryKey];
-
-                            // Append UNION before every other SELECT
-                            if ($i > 0) {
-                                $sql .= ' UNION ';
-                            }
-
-                            if (!in_array($categoryKey, $tableWhitelist[$tableName])) {
-                                throw new Exception('"'.$categoryKey.'" is not whitelisted!');
-                            }
-
-                            $sql .= 'SELECT '.$dbh->quote($categoryName).' AS "name", (SELECT count(*) FROM '.$tableName.' WHERE '.$categoryKey.' = true) AS "quantity"';
+                        // Append UNION before every other SELECT
+                        if ($i > 0) {
+                            $sql .= ' UNION ';
                         }
 
-                        $sql .= ' ORDER BY "quantity" DESC';
+                        if (!in_array($categoryKey, $tableWhitelist[$tableName])) {
+                            throw new Exception('"'.$categoryKey.'" is not whitelisted!');
+                        }
+
+                        $sql .= 'SELECT '.$dbh->quote($categoryName).' AS "name", (SELECT count(*) FROM '.$tableName.' WHERE '.$categoryKey.' = true) AS "quantity"';
                     }
 
-                    break;
-                default:
-                    throw new Exception('"'.$rankingType.'" is a valid ranking type!');
-                    break;
-            }
+                    $sql .= ' ORDER BY "quantity" DESC';
+                }
 
-            $stmt = $dbh->prepare($sql);
-
-            if (!$stmt->execute()) {
-                throw new PDOException($stmt->errorInfo()[2]);
-            }
-
-            $rankings = $stmt->fetchAll(PDO::FETCH_BOTH);
-
-            echo get_rankings_html($rankings);
+                break;
+            default:
+                throw new Exception('"'.$rankingType.'" is a valid ranking type!');
+                break;
         }
+
+        $stmt = $dbh->prepare($sql);
+
+        if (!$stmt->execute()) {
+            throw new PDOException($stmt->errorInfo()[2]);
+        }
+
+        $rankings = $stmt->fetchAll(PDO::FETCH_BOTH);
+
+        echo get_rankings_html($rankings);
     }
