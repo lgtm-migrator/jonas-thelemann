14e0a0c53dce10c40dd5ed9bf8022e6ba3edb458
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-12-11 19:27:44 +0100

    Inline Small JavaScript
    ~ Inline options.js and tagmanager.min.js
    ~ Use minimized functions.js
    - Inlined code files

--- src/static/resources/dargmuesli/url/require.php
+++ src/static/resources/dargmuesli/url/require.php
@@ -13,13 +13,8 @@
             'base' => array(
                 'func.js' => '/base/functions.js',
                 'func.mjs' => '/base/functions.min.js',
-                'opt.js' => '/base/options.js',
                 'stl.css' => '/base/style.css',
                 'stl.mcss' => '/base/style.min.css',
-            ),
-            'gtm' => array(
-                'js' => '/google-tagmanager/tagmanager.js',
-                'mjs' => '/google-tagmanager/tagmanager.min.js',
             ),
         ),
         'pkg' => array(
