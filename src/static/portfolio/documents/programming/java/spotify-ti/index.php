22b7ae2a62dfaaa3c1cab1f693bed2a8a655896b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-01-02 00:47:31 +0100

    Spotify TI Code Fix
    ~ Adapt to return url changes

--- src/static/portfolio/documents/programming/java/spotify-ti/index.php
+++ src/static/portfolio/documents/programming/java/spotify-ti/index.php
@@ -7,10 +7,27 @@
     $skeletonDescription = 'Infomationen über mein Java-Projekt \'SpotifyTI\'';
     $skeletonFeatures = ['lcl/ext/js'];
     $skeletonContent = '
-    <div id="var" class="row">
+    <div class="row">
         <p>
             Dieses Projekt befindet sich in der Entwicklungsphase.
             Es wird dazu dienen, Informationen von der <a href="https://developer.spotify.com/documentation/web-api/" rel="noopener" title="Web-API" target="blank">Spotify Web-API</a> zu holen und diese lokalen Dateien zuzuordnen.
+        </p>
+    </div>
+    <div id="hidden" class="hidden">
+        <h2>
+            Dein Code
+        </h2>
+        <p>
+            Kopiere ihn als nächstes und füge ihn in SpotifyTI ein.
+        </p>
+        <div class="card-panel"><span id="code"></span></div>
+        <p>
+            <a class="btn copy waves-effect waves-light" href="#!" id="copy">
+                <i class="material-icons left">
+                    content_copy
+                </i>
+                Kopieren
+            </a>
         </p>
     </div>';
 
--- src/static/portfolio/documents/programming/java/spotify-ti/layout/extension/extension.js
+++ src/static/portfolio/documents/programming/java/spotify-ti/layout/extension/extension.js
@@ -1,30 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var hashElement = document.getElementById('var');
-    var hashParams = Dargmuesli.Prototyping.getHashParams();
+    let url = new URL(window.location.href);
+    let code = url.searchParams.get('code');
 
-    if (Object.keys(hashParams).length !== 0 || hashParams.constructor !== Object) {
-        hashElement.innerHTML = `
-        <h2>
-            Dein Code
-        </h2>
-        <p>
-            <strong>
-                Er läuft in ` + hashParams['expires_in'] + ` Sekunden ab.
-            </strong>
-            Kopiere ihn als nächstes und füge ihn in SpotifyTI ein.
-        </p>
-        <div class="card-panel"><span id="hash">` + hashParams['access_token'] + `</span></div>
-        <p>
-            <a class="btn copy waves-effect waves-light" href="` + window.location.hash + `">
-                <i class="material-icons left">
-                    content_copy
-                </i>
-                Kopieren
-            </a>
-        </p>`;
+    if (code) {
+        document.querySelector('#code').innerHTML = code;
     }
 
-    document.querySelectorAll('.copy').forEach(function (element) {
-        element.addEventListener('click', function () { Dargmuesli.Prototyping.copy('#hash') });
-    });
+    document.querySelector('#copy').addEventListener('click', function () { Dargmuesli.Prototyping.copy('#code') });
+    document.querySelector('#hidden').classList.toggle('hidden');
 });
