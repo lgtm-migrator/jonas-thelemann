e88e8439f32cb00d0fd10269a51fd290aa8c2eb6
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-03-13 16:09:23 +0100

    Hotfix: Dotenv
    ~ Correct constructor

--- src/static/resources/dargmuesli/filesystem/environment.php
+++ src/static/resources/dargmuesli/filesystem/environment.php
@@ -1,7 +1,7 @@
 <?php
     function load_env_file($folder, $file = '.env', $override = false)
     {
-        $dotenv = new Dotenv\Dotenv($folder, $file);
+        $dotenv = Dotenv\Dotenv::create($folder, $file);
         ($override) ? $dotenv->overload() : $dotenv->load();
         return $dotenv;
     }
