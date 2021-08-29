c3799d297e1f60290683333c7374480bb10c9edf
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-08-29 23:55:37 +0200

    fix(i18n): add header

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <main>
+      <nuxt />
+    </main>
+    <!-- <Footer /> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  head() {
+    return this.$nuxtI18nHead({ addSeoAttributes: true })
+  },
+  beforeCreate() {
+    this.$moment.locale(this.$i18n.locale)
+  },
+})
+</script>
